752cd62c49b0960ef8c6cacddad547d277d9993b
Jeongdong <59277907+poplar0713@users.noreply.github.com>
2023-12-12 13:28:16 +0900

    project init

---
+++ src/test/java/com/ptcourse/example/PtCourseBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptcourse.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtCourseBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
